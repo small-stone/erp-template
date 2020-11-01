b71e8ed19cb4a1e836cfcf44f5c77e85694310d2
小石头 <342206015@qq.com>
2020-11-01 23:06:31 +0800

    feat:修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,124 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider
+        :style="{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }"
+        v-model:collapsed="collapsed"
+        :trigger="null"
+        collapsible
+      >
+        <div class="logo">erp-template</div>
+        <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+          <a-menu-item key="1">
+            <user-outlined />
+            <span>nav 1</span>
+          </a-menu-item>
+          <a-sub-menu key="2">
+            <template v-slot:title>
+              <span><user-outlined /><span>User</span></span>
+            </template>
+            <a-menu-item key="3">Tom</a-menu-item>
+            <a-menu-item key="4">Bill</a-menu-item>
+            <a-menu-item key="5">Alex</a-menu-item>
+          </a-sub-menu>
+          <a-menu-item key="3">
+            <upload-outlined />
+            <span>nav 3</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout :style="{ marginLeft: '200px' }">
+        <a-layout-header style="background: #fff; padding: 0">
+          <menu-unfold-outlined
+            v-if="collapsed"
+            class="trigger"
+            @click="() => (collapsed = !collapsed)"
+          />
+          <menu-fold-outlined
+            v-else
+            class="trigger"
+            @click="() => (collapsed = !collapsed)"
+          />
+        </a-layout-header>
+        <a-layout-content
+          :style="{
+            margin: '24px 16px',
+            padding: '24px',
+            background: '#fff',
+            minHeight: '280px',
+          }"
+        >
+          <router-view />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
   </div>
-  <router-view/>
 </template>
 
-<style lang="less">
+<script>
+import "ant-design-vue/dist/antd.css";
 
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons-vue";
+
+export default {
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  },
+  data() {
+    return {
+      selectedKeys: ["1"],
+      collapsed: false,
+    };
+  },
+};
+</script>
+
+<style lang="less">
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+
+.ant-menu-submenu-open.ant-menu-submenu-inline {
+  & > .ant-menu-submenu-title {
+    .ant-menu-submenu-arrow {
+      transform: translateY(0px) rotate(-180deg);
+      transform-origin: 30% center;
+      &::before {
+        transform: rotate(-45deg) translateX(2px);
+      }
+      &::after {
+        transform: rotate(45deg) translateX(-2px);
+      }
+    }
+  }
+}
 </style>
